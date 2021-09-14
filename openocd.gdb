aa85153cb5c68dd790023f802cb6d8d80bbfb94a
Cristian Eigel <cristian.eigel@esrlabs.com>
2021-09-14 12:14:40 +0200

    Add support for 7 digit display

--- openocd.gdb
+++ openocd.gdb
@@ -11,12 +11,13 @@ set backtrace limit 32
 # break HardFault
 break rust_begin_unwind
 
+break main
 # *try* to stop at the user entry point (it might be gone due to inlining)
 
 # # send captured ITM to the file itm.fifo
 # # (the microcontroller SWO pin must be connected to the programmer SWO pin)
 # # 8000000 must match the core clock frequency
-monitor tpiu config internal itm.txt uart off 72000000
+monitor tpiu config internal itm.txt uart off 168000000
 
 # # OR: make the microcontroller SWO pin output compatible with UART (8N1)
 # # 8000000 must match the core clock frequency
--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,14 @@
 #![no_main]
 extern crate heapless;
 
-use panic_itm as _;
+use panic_rtt_target as _;
 
-use cortex_m_log::destination;
-use cortex_m_log::log::Logger;
-use cortex_m_log::printer::itm::InterruptSync;
 use feather_f405::hal as stm32f4xx_hal;
 use heapless::String;
-use log::info;
+use ht16k33::{Dimming, Display, HT16K33};
 use rtic::app;
+use rtt_target::{rprintln, rtt_init_print};
 use stm32f4xx_hal::prelude::*;
-
-static mut LOGGER: Option<Logger<InterruptSync>> = None;
-const LOG_LEVEL: log::LevelFilter = log::LevelFilter::Debug;
 
 #[app(device = feather_f405::hal::stm32, monotonic = rtic::cyccnt::CYCCNT, peripherals = true)]
 const APP: () = {
@@ -28,14 +23,30 @@ const APP: () = {
         core.DWT.enable_cycle_counter();
         core.SCB.set_sleepdeep();
 
-        let logger: &Logger<InterruptSync> = unsafe {
-            LOGGER.replace(Logger {
-                inner: InterruptSync::new(destination::itm::Itm::new(core.ITM)),
-                level: LOG_LEVEL,
-            });
-            LOGGER.as_ref().expect("to have a logger")
-        };
-        cortex_m_log::log::init(logger).expect("To set logger");
+        let device = cx.device;
+        let mut rcc = device.RCC.constrain();
+        let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
+
+        rtt_init_print!();
+        let gpiob = device.GPIOB.split();
+        let scl = gpiob.pb8.into_alternate_af4().set_open_drain();
+        let sda = gpiob.pb7.into_alternate_af4().set_open_drain();
+        let i2c = I2c::i2c1(device.I2C1, (scl, sda), 400.khz(), clocks);
+        const DISP_I2C_ADDR: u8 = 112;
+        let mut ht16k33 = HT16K33::new(i2c, DISP_I2C_ADDR);
+        ht16k33.initialize().expect("Failed to initialize ht16k33");
+        ht16k33
+            .set_display(Display::ON)
+            .expect("Could not turn on the display!");
+        ht16k33
+            .set_dimming(Dimming::BRIGHTNESS_MIN)
+            .expect("Could not set dimming!");
+
+        // Sending individual digits
+        ht16k33.update_buffer_with_digit(Index::One, 1);
+        ht16k33.update_buffer_with_digit(Index::Two, 2);
+        ht16k33.update_buffer_with_digit(Index::Three, 3);
+        ht16k33.update_buffer_with_digit(Index::Four, 4);
         cx.spawn.say_hello().expect("To start say hello task");
         init::LateResources {
             message: "Hello world".into(),
@@ -45,13 +56,13 @@ const APP: () = {
     #[allow(clippy::empty_loop)]
     #[idle()]
     fn idle(_cx: idle::Context) -> ! {
-        info!("idle");
+        rprintln!("idle");
         loop {}
     }
 
     #[task(resources=[message])]
     fn say_hello(cx: say_hello::Context) {
-        info!("{}", cx.resources.message);
+        rprintln!("{}", cx.resources.message);
     }
     extern "C" {
         fn UART4();
