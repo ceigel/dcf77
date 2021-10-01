99bb76cf2f3d7f6baaae226cd255f29298ea7ef0
Cristian Eigel <cristian.eigel@esrlabs.com>
2021-10-01 22:28:46 +0200

    Add binning logic

--- src/dcf77_decoder.rs
+++ src/dcf77_decoder.rs
@@ -8,20 +8,105 @@ pub enum DecoderError {
     WrongTransition,
 }
 
-pub struct DCF77Decoder {
+#[derive(PartialEq, Debug, Clone, Copy)]
+enum Edge {
+    Up,
+    Down,
+}
+
+impl Edge {
+    pub fn new(low_to_high: bool) -> Self {
+        if low_to_high {
+            Edge::Up
+        } else {
+            Edge::Down
+        }
+    }
+}
+
+struct Binning {
     bins: [i8; 1000],
+    max_index: Option<usize>,
+    max_val: i8,
+    min_index: Option<usize>,
+    min_val: i8,
+}
+
+impl Default for Binning {
+    fn default() -> Self {
+        Binning {
+            bins: [0; 1000],
+            max_index: None,
+            max_val: 0,
+            min_index: None,
+            min_val: 0,
+        }
+    }
+}
+
+impl Binning {
+    const MARGIN: usize = 15;
+    pub fn add_edge(&mut self, bin: u32, edge: Edge) -> Option<Edge> {
+        let bin = bin as usize;
+        if edge == Edge::Up {
+            self.bins[bin] += 1;
+            if self.bins[bin] > self.max_val {
+                self.max_val = self.bins[bin];
+                self.max_index.replace(bin);
+            }
+        } else {
+            self.bins[bin] -= 1;
+            if self.bins[bin] < self.min_val {
+                self.min_val = self.bins[bin];
+                self.min_index.replace(bin);
+            }
+            self.scale_bins_if_needed();
+        }
+        self.rate_edge(bin, edge)
+    }
+
+    fn scale_bins_if_needed(&mut self) {
+        if self.max_val >= i8::MAX || self.min_val <= (i8::MIN >> 1) {
+            self.max_val = self.max_val >> 1;
+            self.min_val = self.min_val >> 1;
+            for mut b in self.bins {
+                b = b >> 1;
+            }
+        }
+    }
+
+    fn rate_edge(&self, bin: usize, edge: Edge) -> Option<Edge> {
+        if let (Some(max_index), Some(min_index)) = (self.max_index, self.min_index) {
+            if edge == Edge::Up {
+                if (((max_index as i32) - (bin as i32)).abs() as usize) < Self::MARGIN {
+                    // up-edge close to min (15ms)
+                    return Some(edge);
+                }
+            } else {
+                if (((min_index as i32) - (bin as i32)).abs() as usize) < Self::MARGIN {
+                    // down-edge close to min (15ms)
+                    return Some(edge);
+                }
+            }
+        }
+        None
+    }
+}
+
+pub struct DCF77Decoder {
     last_high_to_low: Option<Instant>,
     last_low_to_high: Option<Instant>,
     cycles_computer: CyclesComputer,
+    bins: Binning,
 }
 
 impl DCF77Decoder {
     pub fn new(cycles_computer: CyclesComputer) -> Self {
         Self {
-            bins: [0; 1000],
             last_high_to_low: None,
             last_low_to_high: None,
             cycles_computer,
+            bins: Binning::default(),
         }
     }
 
@@ -29,7 +114,13 @@ impl DCF77Decoder {
         &mut self,
         low_to_high: bool,
         now: Instant,
+        bin_idx: u32,
     ) -> Result<(), DecoderError> {
+        let edge = Edge::new(low_to_high);
+        if self.bins.add_edge(bin_idx, edge).is_none() {
+            return Ok(());
+        }
+        rprintln!("tick: {:?} {}", edge, bin_idx);
         if low_to_high {
             self.last_low_to_high.replace(now);
             if let Some(last) = self.last_high_to_low.take() {
--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use datetime_converter::DCF77DateTimeConverter;
 use dcf77_decoder::DCF77Decoder;
 use panic_rtt_target as _;
 
+use cast::u16;
 use chrono::naive::NaiveDateTime;
 use cortex_m::peripheral::DWT;
 use cycles_computer::CyclesComputer;
@@ -23,7 +24,7 @@ use rtcc::Rtcc;
 use rtic::app;
 use rtt_target::{rprintln, rtt_init_print};
 use stm32f4xx_hal::gpio::{gpioa, gpiob, AlternateOD, Edge, Input, PullUp, AF4};
-use stm32f4xx_hal::rtc::Rtc;
+use stm32f4xx_hal::{rcc::*, rtc::Rtc};
 use time_display::{display_error, show_rtc_time};
 
 type SegmentDisplay =
@@ -33,12 +34,47 @@ fn sync_rtc(rtc: &mut Rtc, dt: &NaiveDateTime) {
     rtc.set_datetime(dt).expect("To be able to set datetime");
 }
 
+fn enable_tim1(rcc: &mut pac::RCC) {
+    pac::TIM1::enable(rcc);
+    pac::TIM1::reset(rcc);
+}
+
+const ARR_MULTIPL: usize = 1;
+fn start_tim1(tim1: pac::TIM1, clocks: &Clocks) -> pac::TIM1 {
+    // pause
+    tim1.cr1.modify(|_, w| w.cen().clear_bit());
+    // reset counter
+    tim1.cnt.reset();
+
+    let ticks = clocks.pclk2().0; // for 1.hz() = 84 * 1E+6
+
+    // let arr = u16(ticks / u32(psc + 1)).unwrap();
+    let arr: u32 = 1000; // 1000 bins
+
+    let arr = arr << ARR_MULTIPL; // we can't fit more into psc
+    let psc = u16((ticks / arr) - 1).unwrap(); // 42000
+    tim1.psc.write(|w| w.psc().bits(psc));
+
+    tim1.arr.write(|w| unsafe { w.bits(arr) });
+
+    // Trigger update event to load the registers
+    tim1.cr1.modify(|_, w| w.urs().set_bit());
+    tim1.egr.write(|w| w.ug().set_bit());
+    tim1.cr1.modify(|_, w| w.urs().clear_bit());
+
+    // start counter
+    tim1.cr1.modify(|_, w| w.cen().set_bit());
+
+    tim1
+}
+
 const DISP_I2C_ADDR: u8 = 0x77;
 #[app(device = feather_f405::hal::stm32, monotonic = rtic::cyccnt::CYCCNT, peripherals = true)]
 const APP: () = {
     struct Resources {
         segment_display: SegmentDisplay,
-        dcf_pin: gpioa::PA<Input<PullUp>>,
+        dcf_pin: gpioa::PA6<Input<PullUp>>,
+        tim1: pac::TIM1,
         cycles_computer: CyclesComputer,
         val: u16,
         decoder: DCF77Decoder,
@@ -49,12 +85,13 @@ const APP: () = {
     fn init(cx: init::Context) -> init::LateResources {
         rtt_init_print!();
         let mut core = cx.core;
-        let device = cx.device;
+        let mut device = cx.device;
         core.DCB.enable_trace();
         DWT::unlock();
         core.DWT.enable_cycle_counter();
         //core.SCB.set_sleepdeep();
 
+        enable_tim1(&mut device.RCC);
         let clocks = setup_clocks(device.RCC);
         let mut syscfg = device.SYSCFG.constrain();
         let mut exti = device.EXTI;
@@ -77,17 +114,19 @@ const APP: () = {
             .write_display_buffer()
             .expect("Could not write 7-segment display");
         let gpioa = device.GPIOA.split();
-        let mut pin = gpioa.pa6.into_pull_up_input().downgrade();
+        let mut pin = gpioa.pa6.into_pull_up_input();
         pin.make_interrupt_source(&mut syscfg);
-        pin.trigger_on_edge(&mut exti, Edge::RISING_FALLING);
+        pin.trigger_on_edge(&mut exti, Edge::RisingFalling);
         pin.enable_interrupt(&mut exti);
 
+        let tim1 = start_tim1(device.TIM1, &clocks);
         let rtc = Rtc::new(device.RTC, 255, 127, false, &mut pwr);
         rprintln!("Init successful");
         let cc = CyclesComputer::new(clocks.sysclk());
         init::LateResources {
             segment_display: ht16k33,
             dcf_pin: pin,
+            tim1,
             cycles_computer: cc.clone(),
             val: 0,
             decoder: DCF77Decoder::new(cc),
@@ -103,8 +142,9 @@ const APP: () = {
         loop {}
     }
 
-    #[task(binds = EXTI9_5, priority=2, resources=[dcf_pin, cycles_computer, decoder])]
+    #[task(binds = EXTI9_5, priority=2, resources=[dcf_pin, tim1, cycles_computer, decoder])]
     fn exti9_5(cx: exti9_5::Context) {
+        let tim1 = cx.resources.tim1;
         let dcf_pin = cx.resources.dcf_pin;
         let dcf_interrupted = dcf_pin.check_interrupt();
         dcf_pin.clear_interrupt_pending_bit();
@@ -112,10 +152,11 @@ const APP: () = {
             return;
         }
         let now = Instant::now();
+        let tim1_val: u32 = tim1.cnt.read().bits() >> ARR_MULTIPL;
         let res = cx
             .resources
             .decoder
-            .register_transition(dcf_pin.is_high().unwrap(), now);
+            .register_transition(dcf_pin.is_high(), now, tim1_val);
         if let Err(e) = res {
             rprintln!("Err: {:?}", e);
         }
